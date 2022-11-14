ba312e7ca5e1c92381891cfaa3451f49b577e1db
hahuyhoang <huyhoang24062000@gmail.com>
2022-11-14 15:53:13 +0700

    add tailwind

--- Project/Login.js
+++ Project/Login.js
@@ -1,15 +1,15 @@
 import React from "react";
-import {View,TouchableOpacity,StyleSheet,Image,Text, SafeAreaView} from 'react-native';
+import { View, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
 
-const Login =({navigation})=>{
-   return(
-    <SafeAreaView style={{flex: 1,backgroundColor:'#ccc'}}>
-    <View>
-        <Text>Login</Text>
-        <Text>Login</Text>
-        <Text>Login</Text>
-        </View>
-</SafeAreaView>
+const Login = ({ navigation }) => {
+   return (
+      <SafeAreaView className="bg-orange-300">
+         <View >
+            <Text className="text-base">Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+         </View>
+      </SafeAreaView>
    )
 }
 export default Login
